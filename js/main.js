c438736e762ffd8248fb234995b871bdf729a24e
nandha-techie <nandhawebapps@gmail.com>
2022-11-29 20:54:37 +0530

    responsive boottstrap site

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(function(){
+        $('.bxslider').bxSlider({
+          mode: 'fade',
+          captions: true,
+          controls: true,
+          autoControls: false
+        });
+      });
+
+  });
